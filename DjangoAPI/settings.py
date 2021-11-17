d9fd392342134c91d2e999d9868274f9bb664800
Developers Buck <developers.buck@gmail.com>
2021-11-17 15:59:12 +0530

    Update settings.py

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -17,6 +17,14 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 PHOTO_URL = '/photos/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "photos" )
 
@@ -29,7 +37,7 @@ SECRET_KEY = 'django-insecure-)^wo+(g_m+%$5(gx4ke$kf36y3**jgb1qa8mpi60p$g(_rhjj-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -129,8 +137,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 #STATIC_URL = '/static/'
-
-#DISABLE_COLLECTSTATIC=1
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
