4d7dcd3b82e55b699cfea3317bd3d48059965859
Developers Buck <developers.buck@gmail.com>
2021-11-17 16:36:35 +0530

    changes

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -17,12 +17,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 PHOTO_URL = '/photos/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "photos" )
 
@@ -134,7 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
